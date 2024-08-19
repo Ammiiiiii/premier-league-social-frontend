e93caba41adb44a31f2f02b299d9658ca5853e51
Ammiiiiii <fatimamaijamaa05@gmail.com>
2024-08-20 00:31:01 +0100

    Create Home.js

---
+++ Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Home() {
+    return (
+        <div className="home">
+            <div className="banner">
+                <h1>Welcome to Premier League Social</h1>
+                <p>Stay updated with the latest news, articles, and videos</p>
+            </div>
+            <div className="content-sections">
+                {/* Add carousels, grids, and latest updates */}
+            </div>
+        </div>
+    );
+}
+
+export default Home;
